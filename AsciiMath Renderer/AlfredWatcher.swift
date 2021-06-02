12478a2dd4972b1dfe7502abfa44a4ccda02e8bb
Mr. Pennyworth <mr-pennyworth@users.noreply.github.com>
2021-06-02 11:23:31 +0530

    Change indent to two spaces from the old four

--- AsciiMath Renderer/AlfredWatcher.swift
+++ AsciiMath Renderer/AlfredWatcher.swift
@@ -6,38 +6,38 @@ import CoreFoundation
 
 
 class AlfredWatcher {
-    var swindler: Swindler.State!
-    var onDestroy: (() -> Void)!
-    
-    func start(onAlfredWindowDestroy: @escaping () -> Void) {
-        self.onDestroy = onAlfredWindowDestroy
-        guard AXSwift.checkIsProcessTrusted(prompt: true) else {
-            NSLog("Not trusted as an AX process; please authorize and re-launch")
-            NSApp.terminate(self)
-            return
-        }
+  var swindler: Swindler.State!
+  var onDestroy: (() -> Void)!
 
-        Swindler.initialize().done { state in
-            self.swindler = state
-            self.setupEventHandlers()
-        }.catch { error in
-            NSLog("Fatal error: failed to initialize Swindler: \(error)")
-            NSApp.terminate(self)
-        }
-    }
-    
-    private func isAlfredWindow(window: Window) -> Bool {
-        let bundle = window.application.bundleIdentifier ?? ""
-        let title = window.title.value
-        return (bundle == "com.runningwithcrayons.Alfred" && title == "Alfred")
+  func start(onAlfredWindowDestroy: @escaping () -> Void) {
+    self.onDestroy = onAlfredWindowDestroy
+    guard AXSwift.checkIsProcessTrusted(prompt: true) else {
+      NSLog("Not trusted as an AX process; please authorize and re-launch")
+      NSApp.terminate(self)
+      return
     }
 
-    private func setupEventHandlers() {
-        swindler.on { (event: WindowDestroyedEvent) in
-            if (self.isAlfredWindow(window: event.window)) {
-                NSLog("Alfred window destroyed")
-                self.onDestroy()
-            }
-        }
+    Swindler.initialize().done { state in
+      self.swindler = state
+      self.setupEventHandlers()
+    }.catch { error in
+      NSLog("Fatal error: failed to initialize Swindler: \(error)")
+      NSApp.terminate(self)
     }
+  }
+
+  private func isAlfredWindow(window: Window) -> Bool {
+    let bundle = window.application.bundleIdentifier ?? ""
+    let title = window.title.value
+    return (bundle == "com.runningwithcrayons.Alfred" && title == "Alfred")
+  }
+
+  private func setupEventHandlers() {
+    swindler.on { (event: WindowDestroyedEvent) in
+      if (self.isAlfredWindow(window: event.window)) {
+        NSLog("Alfred window destroyed")
+        self.onDestroy()
+      }
+    }
+  }
 }
